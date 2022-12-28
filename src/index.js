70a4c770844d25896ea83c9021b2531eefcf1c3a
JosueDeLosSantos <jdd1993@hotmail.com>
2022-12-27 20:30:55 -0400

    Experiment

--- src/index.js
+++ src/index.js
@@ -19,15 +19,41 @@ const spanCenter3 = document.querySelector(".spanCenter3");
 main.appendChild(centerDiv);
 main.removeChild(form);
 
-window.showForm = function () {
+function showForm() {
   main.removeChild(centerDiv);
   main.appendChild(form);
-};
 
-window.hideForm = function () {
+  let titleInput =
+    document.body.children[1].children[0].children[0].children[2];
+  let descriptionInput =
+    document.body.children[1].children[0].children[0].children[4];
+  let dateInput = document.body.children[1].children[0].children[0].children[6];
+  let priorityInput =
+    document.body.children[1].children[0].children[0].children[8];
+
+  titleInput.value = "";
+  descriptionInput.value = "";
+  dateInput.value = "";
+  priorityInput.value = "";
+}
+window.showForm = showForm;
+
+let taskRepo = [];
+
+function hideForm(e) {
   main.appendChild(centerDiv);
   main.removeChild(form);
-};
+
+  let title = e.target.parentNode.children[2].value;
+  let description = e.target.parentNode.children[4].value;
+  let date = e.target.parentNode.children[6].value;
+  let priority = e.target.parentNode.children[8].value;
+
+  let task = { title, description, date, priority };
+  taskRepo.push(task);
+  console.log(taskRepo);
+}
+window.hideForm = hideForm;
 
 spanCenter3.addEventListener("click", showForm);
 button.addEventListener("click", hideForm);
