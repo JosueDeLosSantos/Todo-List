a0ff97b207859098f8223752b062df088d59324c
JosueDeLosSantos <jdd1993@hotmail.com>
2022-12-30 11:28:58 -0400

    Add navigation bar to all sections.

---
+++ src/taskList/tasks.js
@@ -0,0 +1,8 @@
+import "../index.css";
+import "../form.css";
+import pinIcon from "../icons/pin.png";
+
+const logoIcon = document.querySelector(".logoIcon");
+const logoPinIcon = new Image();
+logoPinIcon.src = pinIcon;
+logoIcon.appendChild(logoPinIcon);
